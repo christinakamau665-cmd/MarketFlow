a8b80a451d067bda4c77804df1247e4886404d63
christinakamau665-cmd <christinakamau665@gmail.com>
2026-07-16 11:29:26 +0300

    My third commit

--- supermarket_project/settings.py
+++ supermarket_project/settings.py
@@ -35,7 +35,7 @@ ROOT_URLCONF = 'supermarket_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'supermarket' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -70,6 +70,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'supermarket' / 'static']
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
